1f545ee7d09a2f5035c925a7b9ae196450aa38a9
Jorge Avila <avilajorge314@gmail.com>
2019-02-07 23:59:28 -0800

    Moved code from tb to compile in Quartus.  Working version with 151 cycles, FMax: 161.03 MHz, Comb ALUTs: 1356, registers: 1130

--- src/simplified_sha256.sv
+++ src/simplified_sha256.sv
@@ -14,7 +14,7 @@ module simplified_sha256(input logic clk, reset_n, start,
   	// State set up and other variables
   	enum logic [3:0] {IDLE, INIT, WAIT, COMPUTE, READ, WRITE, DONE, POST} state;
   	logic [31:0] block_counter;
-	logic [15:0] in_addr, out_addr, t_counter;
+	logic [15:0] t_counter;
 	logic [31:0] H[0:7];
 	logic [31:0] a, b, c, d, e, f, g, h;
 	
@@ -23,6 +23,42 @@ module simplified_sha256(input logic clk, reset_n, start,
 	 32'h428a2f98, 32'h71374491, 32'hb5c0fbcf, 32'he9b5dba5, 32'h3956c25b, 32'h59f111f1, 32'h923f82a4, 32'hab1c5ed5,
 	 32'hd807aa98, 32'h12835b01, 32'h243185be, 32'h550c7dc3, 32'h72be5d74, 32'h80deb1fe, 32'h9bdc06a7, 32'hc19bf174
 	};
+	
+		// SHA256 K constants
+	parameter int k[0:63] = '{
+	   32'h428a2f98,32'h71374491,32'hb5c2fbcf,32'he9b5dba5,32'h3956c25b,32'h59f111f1,32'h923f82a4,32'hab1c5ed5,
+	   32'hd807aa98,32'h12835b01,32'h243185be,32'h550c7dc3,32'h72be5d74,32'h80deb1fe,32'h9bdc06a7,32'hc19bf174,
+	   32'he49b69c1,32'hefbe4786,32'h0fc19dc6,32'h240ca1cc,32'h2de92c6f,32'h4a7484aa,32'h5cb0a9dc,32'h76f988da,
+	   32'h983e5152,32'ha831c66d,32'hb00327c8,32'hbf597fc7,32'hc6e00bf3,32'hd5a79147,32'h06ca6351,32'h14292967,
+	   32'h27b70a85,32'h2e1b2138,32'h4d2c6dfc,32'h53380d13,32'h650a7354,32'h766a0abb,32'h81c2c92e,32'h92722c85,
+	   32'ha2bfe8a1,32'ha81a664b,32'hc24b8b70,32'hc76c51a3,32'hd192e819,32'hd6990624,32'hf40e3585,32'h106aa070,
+	   32'h19a4c116,32'h1e376c08,32'h2748774c,32'h34b0bcb5,32'h391c0cb3,32'h4ed8aa4a,32'h5b9cca4f,32'h682e6ff3,
+	   32'h748f82ee,32'h78a5636f,32'h84c87814,32'h8cc70208,32'h90befffa,32'ha4506ceb,32'hbef9a3f7,32'hc67178f2
+	};
+
+	// SHA256 hash round
+	function logic [255:0] sha256_op(input logic [31:0] a, b, c, d, e, f, g, h, w,
+	                                 input logic [7:0] t);
+	    logic [31:0] S1, S0, ch, maj, t1, t2; // internal signals
+	begin
+	    S1 = rightrotate(e, 6) ^ rightrotate(e, 11) ^ rightrotate(e, 25);
+	    ch = (e & f) ^ ((~e) & g);
+	    t1 = h + S1 + ch + k[t] + w;
+	    S0 = rightrotate(a, 2) ^ rightrotate(a, 13) ^ rightrotate(a, 22);
+	    maj = (a & b) ^ (a & c) ^ (b & c);
+	    t2 = S0 + maj;
+	
+	    sha256_op = {t1 + t2, a, b, c, d + t1, e, f, g};
+	end
+	endfunction
+	
+	// right rotation
+	function logic [31:0] rightrotate(input logic [31:0] x,
+	                                  input logic [7:0] r);
+	begin
+	    rightrotate = (x >> r) | (x << (32-r));
+	end
+	endfunction
 	
 	//The function that gives new w[15]
 	function logic [31:0] wtnew; // function with no inputs     
@@ -51,9 +87,6 @@ module simplified_sha256(input logic clk, reset_n, start,
 				//Wait for start signal
 				if (start) begin
 					state <= INIT;
-					//Save the info
-//					in_addr <= message_addr;
-//					out_addr <= output_addr;
 					t_counter <= 0;
 					
 					//Init the 8 regs
@@ -141,7 +174,7 @@ module simplified_sha256(input logic clk, reset_n, start,
 		 			mem_we <= 1;
 		 			mem_addr <= output_addr + t_counter;
 		 			t_counter <= t_counter + 1;
-		 			state = WRITE;
+		 			state <= WRITE;
 	 			end else begin
 		 			state <= DONE;
  				end
